939798f41ffbb2218805184b1c82e4bc6b6711e6
Kiel DeMarco <73503331+KielDemarco@users.noreply.github.com>
2021-03-28 05:18:00 -0700

    Update variables.tf

--- iam_user/variables.tf
+++ iam_user/variables.tf
@@ -4,5 +4,5 @@ variable "region" {
 
 variable "username" {
   type    = list(string)
-  default = ["ec2-Beta", "ec2-Charlie", "ec2-Delta", "ec2-Epsilon", "ec2-Foxtrot", "ec2-Golf", "ec2-Harry"]
+  default = ["", "", "", "", "", "", ""]
 }
